e39e2816c697ab68edb615faca76f6939032ccdb
A-GORE <gonzalezreyesadriano@gmail.com>
2024-10-07 12:55:52 -0600

    min_length

---
+++ min_length/min_length.py
@@ -0,0 +1,11 @@
+class Solution:
+    def minLength(self, s: str) -> int:
+        while 'AB' in s or 'CD' in s:
+            if 'AB' in s:
+                s = s.replace('AB', '')
+            if 'CD' in s:
+                s = s.replace('CD', '')
+        return len(s)
+
+s = Solution()
+print(s.minLength("ABFCACDB"))
